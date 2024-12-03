a3ccc1b994798eff9c7cf16c256c7b8f5e89bc77
agnesl10 <agnesl10@nycstudents.net>
2024-12-03 12:31:45 -0500

    sdfsdf

--- app/JS/main.js
+++ app/JS/main.js
@@ -29,11 +29,11 @@ function insertData(agents) {
   agents.forEach((agent) =>
     DOMSelectors.container.insertAdjacentHTML(
       "beforeend",
-      `<div class="card w-4/5 h-5/5 p-2.5 m-5 bg-red-400 text-amber-950 rounded-s-lg flex flex-col overflow-hidden justify-around">
-        <h2 class="title">${agent.displayName}</h2>
-        <img class="image " src="${agent.fullPortrait}" alt="" />
-        <h3 class="role">${agent.role.displayName}</h3>
-        <h3 class="desc">${agent.description}</h3>
+      `<div class="card w-2/6 h-3/6 p-2.5 m-8 mx-0.5 bg-red-400 text-amber-950 rounded-s-lg flex flex-col overflow-hidden justify-around items-center">
+        <h2 class="title text-2xl">${agent.displayName}</h2>
+        <h3 class="role text-base">${agent.role.displayName}</h3>
+        <img class="image" src="${agent.fullPortrait}" alt="" />
+        <h3 class="desc text-base">${agent.description}</h3>
       </div>`
     )
   );
